6d5f1eb802f0aa465c8f9074e39bd2bff398828f
Samson Akanbi <samson.akanbi@outlook.com>
2018-11-13 23:07:43 +0100

    Added the Icons page.

--- Wrappixel Material Lite/Controllers/HomeController.cs
+++ Wrappixel Material Lite/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Wrappixel_Material_Lite.Controllers
             return View();
         }
 
+        public ActionResult Icons()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
